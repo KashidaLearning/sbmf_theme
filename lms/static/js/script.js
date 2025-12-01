151ed164a2ae9c4913f7e0065a4325f4d72587b4
msscompany1 <m.soboh@bau.edu.lb>
2025-12-01 17:42:20 +0200

    fix responsive issues

--- lms/static/js/script.js
+++ lms/static/js/script.js
@@ -41,7 +41,7 @@ function createCircle(data, index) {
         circle.innerHTML = `
             <div class="circle-content">
                 <div class="state-icon state-icon--${state}">
-                    <img width="60" src="${iconToUse}">
+                    <img width="100%" src="${iconToUse}">
                 </div>
                 <div class="circle-text-content">
                     <div class="circle-title">${data.title}</div>
@@ -57,7 +57,7 @@ function createCircle(data, index) {
         circle.innerHTML = `
             <div class="circle-content">
                 <div class="state-icon2 state-icon--${state}">
-                    <img width="60" src="${iconToUse}">
+                    <img width="100%" src="${iconToUse}">
                 </div>
                 <div class="circle-image-wrapper2">
                     <div class="circle-image-container2">
