10ad734f09c7e5fa85eebc1032555ad65d33e9d6
Ubuntu <ubuntu@ip-172-31-25-68.eu-west-1.compute.internal>
2026-06-26 15:10:23 +0000

    Add password visibility toggle

--- lms/static/js/script.js
+++ lms/static/js/script.js
@@ -973,3 +973,16 @@ function applyPulseToActiveCourses() {
     });
 }
 document.addEventListener("circlesRebuilt", applyPulseToActiveCourses);
+$(document).on("click", ".password-toggle", function () {
+    var targetId = $(this).data("target");
+    var input = $("#" + targetId);
+    var icon = $(this).find("i");
+
+    if (input.attr("type") === "password") {
+        input.attr("type", "text");
+        icon.removeClass("fa-eye").addClass("fa-eye-slash");
+    } else {
+        input.attr("type", "password");
+        icon.removeClass("fa-eye-slash").addClass("fa-eye");
+    }
+});
